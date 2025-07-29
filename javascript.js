57ac7d83a2dc0e860f0ec5fa7313ba10019e1a59
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-07-30 01:19:06 +0200

    feat: Add overly complex function which I will get rid of next time

--- javascript.js
+++ javascript.js
@@ -3,22 +3,46 @@ const Gameboard = (function () {
     for (let index = 0; index < 9; index++) {
         gameboard.push(null);
     }
-    function getGameboard() {
-        return gameboard;
+    function getBoard() {
+        return Array.from(gameboard);
     }
     function setCell(cell, mark) {
         if (cell >= 0 && cell <= gameboard.length - 1) {
             gameboard[cell] = mark;
         }
     }
-    return { getGameboard, setCell };
+    function isWinningState(mark) {
+        const markedCells = [];
+
+        // as much as it hurts, get rid of this "smart" logic, as you did not consider additional entries );
+        let winningState = false;
+        gameboard.forEach((cell, index) => {
+            if (cell == mark) {
+                markedCells.push(index);
+            }
+        });
+        if (Math.sqrt(gameboard.length) == markedCells.length) {
+            const initialDifference =
+                markedCells[markedCells.length - 1] -
+                markedCells[markedCells.length - 2];
+            winningState = markedCells.reduceRight((prevCell, cell) => {
+                if (prevCell - initialDifference == cell) {
+                    return cell;
+                } else {
+                    return false;
+                }
+            }, markedCells[markedCells.length - 1] + initialDifference);
+        }
+        return winningState !== false ? true : false;
+    }
+    return { getBoard, setCell, isWinningState };
 })();
 
 const Display = (function () {
-    function printToConsole() {
-        console.log(Gameboard.getGameboard());
+    function printGameboard() {
+        console.log(Gameboard.getBoard());
     }
-    return { printToConsole };
+    return { printGameboard };
 })();
 
 function createPlayer(name) {
@@ -33,10 +57,15 @@ function createPlayer(name) {
         },
     };
 }
-const player1 = createPlayer("Josh");
-const player2 = createPlayer("Joshephine");
 
-Gameboard.setCell(8, "X");
-Gameboard.setCell(-1, "X");
-Gameboard.setCell(5, "0");
-Display.printToConsole();
+const Gameflow = (function () {
+    const player1 = createPlayer("Josh");
+    const player2 = createPlayer("Joshephine");
+})();
+
+Gameboard.setCell(2, "Y");
+Gameboard.setCell(0, "Y");
+Gameboard.setCell(1, "Y");
+console.log(Gameboard.isWinningState("Y"));
+Gameboard.getBoard()[3] = 1;
+Display.printGameboard();
