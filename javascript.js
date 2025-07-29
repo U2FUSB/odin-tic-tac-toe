f891f312a71de9791430fd3087f4a1f074ded364
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-07-29 23:31:03 +0200

    feat: Add createPlayer factory

--- javascript.js
+++ javascript.js
@@ -14,7 +14,29 @@ const Gameboard = (function () {
     return { getGameboard, setCell };
 })();
 
+const Display = (function () {
+    function printToConsole() {
+        console.log(Gameboard.getGameboard());
+    }
+    return { printToConsole };
+})();
+
+function createPlayer(name) {
+    let wins = 0;
+    return {
+        name,
+        getWins: function () {
+            return wins;
+        },
+        increaseWins: function () {
+            wins++;
+        },
+    };
+}
+const player1 = createPlayer("Josh");
+const player2 = createPlayer("Joshephine");
+
 Gameboard.setCell(8, "X");
 Gameboard.setCell(-1, "X");
 Gameboard.setCell(5, "0");
-console.log(Gameboard.getGameboard());
+Display.printToConsole();
