963a561dbb6bcff600e80299b6fe44df5afd7322
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-07-31 20:29:02 +0200

    feat: Add isWinningMark

--- javascript.js
+++ javascript.js
@@ -11,31 +11,33 @@ const Gameboard = (function () {
             gameboard[cell] = mark;
         }
     }
-    function isWinningState(mark) {
-        const markedCells = [];
-
-        // as much as it hurts, get rid of this "smart" logic, as you did not consider additional entries );
-        let winningState = false;
-        gameboard.forEach((cell, index) => {
-            if (cell == mark) {
-                markedCells.push(index);
+    function isWinningMark(mark) {
+        let isWinning = false;
+        const winningStates = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        winningStates.forEach((winningState) => {
+            const winningCount = 3;
+            let currentCount = 0;
+            winningState.forEach((winningStateElement) => {
+                if (getBoard().at(winningStateElement) == mark) {
+                    currentCount++;
+                }
+            });
+            if (currentCount == winningCount) {
+                isWinning = true;
             }
         });
-        if (Math.sqrt(gameboard.length) == markedCells.length) {
-            const initialDifference =
-                markedCells[markedCells.length - 1] -
-                markedCells[markedCells.length - 2];
-            winningState = markedCells.reduceRight((prevCell, cell) => {
-                if (prevCell - initialDifference == cell) {
-                    return cell;
-                } else {
-                    return false;
-                }
-            }, markedCells[markedCells.length - 1] + initialDifference);
-        }
-        return winningState !== false ? true : false;
+        return isWinning;
     }
-    return { getBoard, setCell, isWinningState };
+    return { getBoard, setCell, isWinningMark };
 })();
 
 const Display = (function () {
@@ -66,6 +68,6 @@ const Gameflow = (function () {
 Gameboard.setCell(2, "Y");
 Gameboard.setCell(0, "Y");
 Gameboard.setCell(1, "Y");
-console.log(Gameboard.isWinningState("Y"));
+console.log(Gameboard.isWinningMark("Y"));
 Gameboard.getBoard()[3] = 1;
 Display.printGameboard();
